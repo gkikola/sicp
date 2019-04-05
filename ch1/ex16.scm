6209bbd9310c7adb76dfd040dc0e3faa31d14e7b
Greg Kikola <gkikola@gmail.com>
2019-04-05 01:59:24 -0400

    Add Exercise 1.16

---
+++ ch1/ex16.scm
@@ -0,0 +1,17 @@
+;; Exercise 1.16
+;;
+;; (expt b n) computes b^n using an iterative process, with the number
+;; of steps on the order of log(n). n must be nonnegative.
+;;
+;; Within each iteration, a*b^n remains invariant.
+
+(define (even? n) (= (remainder n 2) 0))
+(define (square n) (* n n))
+
+(define (expt b n)
+  (define (iter a b n)
+    (cond
+     ((= n 0) a)
+     ((even? n) (iter a (square b) (/ n 2)))
+     (else (iter (* a b) b (- n 1)))))
+  (iter 1 b n))
