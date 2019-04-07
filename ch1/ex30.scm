4dad0c11981df0f6884af83855de474ace9de8a5
Greg Kikola <gkikola@gmail.com>
2019-04-07 12:25:16 -0400

    Add Exercise 1.30

---
+++ ch1/ex30.scm
@@ -0,0 +1,10 @@
+;; Exercise 1.30
+;;
+;; An iterative version of sum
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
