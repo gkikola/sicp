3561232bd622a4583de68c3bf8fc1a3f81ce8a69
Greg Kikola <gkikola@gmail.com>
2019-04-12 00:36:56 -0400

    Add Exercise 2.10

---
+++ ch2/ex10.scm
@@ -0,0 +1,13 @@
+;; Exercise 2.10
+;;
+;; Interval arithmetic, continued
+
+(load "ex08.scm")
+
+;; Modified division
+(define (div-interval i j)
+  (if (and (< (lower-bound j) 0) (> (upper-bound j) 0))
+      (error "Potential division by 0"))
+  (mul-interval i
+                (make-interval (/ 1.0 (upper-bound j))
+                               (/ 1.0 (lower-bound j)))))
