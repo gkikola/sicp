cf6571629e9226eacfc8b8f8c58dc2e09ec6f8c2
Greg Kikola <gkikola@gmail.com>
2019-04-13 16:18:50 -0400

    Add Exercise 2.21

---
+++ ch2/ex21.scm
@@ -0,0 +1,15 @@
+;; Exercise 2.21
+;;
+;; Squaring elements in a list
+
+(define (square x) (* x x))
+
+;; Squares each element in the given list.
+(define (square-list items)
+  (if (null? items)
+      '()
+      (cons (square (car items)) (square-list (cdr items)))))
+
+;; Alternate implementation using map.
+(define (square-list items)
+  (map square items))
