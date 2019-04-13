9f28d7768ecf43e122ee35eba03291a20c84768c
Greg Kikola <gkikola@gmail.com>
2019-04-13 19:57:11 -0400

    Add Exercise 2.30

---
+++ ch2/ex30.scm
@@ -0,0 +1,19 @@
+;; Exercise 2.30
+;;
+;; Squaring trees
+
+(define (square x) (* x x))
+
+;; Square the leaves of a tree.
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (expt tree 2))
+        (else (cons (square-tree (car tree)) (square-tree (cdr tree))))))
+
+;; Alternate implementation using map.
+(define (square-tree tree)
+  (map (lambda (branch)
+         (if (pair? branch)
+             (square-tree branch)
+             (expt branch 2)))
+       tree))
