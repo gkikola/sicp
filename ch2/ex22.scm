e9a07be6fb9f8b518b8194a4def178106e534209
Greg Kikola <gkikola@gmail.com>
2019-04-13 16:36:24 -0400

    Add Exercise 2.22

---
+++ ch2/ex22.scm
@@ -0,0 +1,8 @@
+;; Exercise 2.22
+;;
+;; For each
+
+;; Call the given procedure on each element of the list.
+(define (for-each proc list)
+  (cond ((null? list) #t)
+        (else (proc (car list)) (for-each proc (cdr list)))))
