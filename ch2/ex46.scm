9160d0009cdac74e9d883264a8dba3dcafc4a5d2
Greg Kikola <gkikola@gmail.com>
2019-04-16 17:36:50 -0400

    Add Exercise 2.46

---
+++ ch2/ex46.scm
@@ -0,0 +1,22 @@
+;; Exercise 2.46
+;;
+;; Vectors
+
+;; Constructor and selectors.
+(define (make-vect x y) (list x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cadr v))
+
+;; Vector addition.
+(define (add-vect v w)
+  (make-vect (+ (xcor-vect v) (xcor-vect w))
+             (+ (ycor-vect v) (ycor-vect w))))
+
+;; Scalar multiplication.
+(define (scale-vect v s)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+;; Vector subtraction.
+(define (sub-vect v w)
+  (add-vect v (scale-vect w -1)))
