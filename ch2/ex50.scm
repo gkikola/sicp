e41059e51f1621c1ae05d18ed2fa83c6bc87140e
Greg Kikola <gkikola@gmail.com>
2019-04-18 13:30:11 -0400

    Add Exercise 2.50

---
+++ ch2/ex50.scm
@@ -0,0 +1,26 @@
+;; Exercise 2.50
+;;
+;; Painter transformations
+
+(load "../util/vector.scm")
+(load "../util/painter/painter.scm")
+
+;; Produces a painter by horizontally flipping the given painter.
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+;; Produces a painter by rotating the given painter counterclockwise.
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
